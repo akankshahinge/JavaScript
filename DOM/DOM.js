edb16c136a342906a36db60412b1cc3e8b45c56e
Akanksha Hinge <70201851+akankshahinge@users.noreply.github.com>
2024-04-05 16:51:32 +0530

    Update DOM.js

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -39,7 +39,6 @@ console.log(title_inner_html) //DOM learning <span style="display: none;">Hello 
 const heading = document.getElementsByClassName("heading");
 console.log("Heading - ", heading);
 
-//className returns HTMLCollection
 const myListClass = document.getElementsByClassName('mylist');  //gives HTML Collection, and which dont have method to loop on it
 console.log("mylist ",myListClass)
 
@@ -65,7 +64,7 @@ const myul = document.querySelector('ul');      //gives enitre list
 const myli = myul.querySelector('li');          //gives first list item
 myli.style.background = "green";
 //------------------------------------------------------------------------------------------------------
-//Query Selector All - Returns NodeList
+//Query Selector All
 const myLi = document.querySelectorAll('li');  //here all li will be in NodeList myLi
 myLi[1].style.background = "green";
 
@@ -73,3 +72,67 @@ myLi.forEach(function (li) {
     li.style.fontFamily = "cursive";
 })
 //-----------------------------------------------------------------------------------------------------
+//Accessing Parent and Child Element
+const parent = document.querySelector('.parent');
+console.log(parent)
+
+//Get the children from parent
+console.log(parent.children)     //returns HTMLCollection
+//We can run a for loop on this parent.children, but if we want to run forEach then convert it into array
+for (let index = 0; index < parent.children.length; index++) {
+    console.log(parent.children[index].innerText);
+}
+
+//Get First Element
+console.log("FirstElementChild ",parent.firstElementChild);
+//Get Last Element
+console.log("LastElementChild ",parent.lastElementChild);
+
+//Get the Parent from Children
+const dayone = document.querySelector('.day');
+console.log(dayone);
+console.log("Parent ",dayone.parentElement);
+
+//Get the next silbing from child
+console.log("Sibling ",dayone.nextElementSibling);
+
+//Get Child Nodes
+console.log("Child Nodes", parent.childNodes);  //It also counts the linebreak and enters
+//-----------------------------------------------------------------------------------------------------------
+//Creating new Element in DOM
+
+const div = document.createElement('div');
+div.id = "myNewId";
+div.className = "myNewClass";
+div.style.backgroundColor = "green";
+div.style.padding = "10px";
+div.innerText = "Sample create node";
+document.body.appendChild(div);
+//-----------------------------------------------------------------------------------------------------------
+//Edit Node, Delete Node
+const my_list= document.querySelector(".myul");
+
+function addNode(text){
+    const my_liElement = document.createElement('li');
+    my_liElement.innerText = text;
+    my_liElement.style.fontFamily = "cursive";
+    my_list.appendChild(my_liElement);
+}
+
+const lang_list = ["Python","Java","GoLang","C++"];
+for (let i = 0; i < lang_list.length; i++) {
+    addNode(lang_list[i])    
+}
+
+//Edit
+const secondLang = document.querySelector("li:nth-child(2)");
+secondLang.innerText = "PHP";
+//or you can use replaceWith to replace previous with new one
+const my_liElement = document.createElement('li');
+my_liElement.innerText = "C";
+secondLang.replaceWith(my_liElement);
+
+//Delete
+const Lang = document.querySelector(".myul");
+const Lang_items = Lang.querySelectorAll("li");
+Lang_items[4].remove();
